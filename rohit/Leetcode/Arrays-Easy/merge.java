542c52c433ef03e40a4a7c7df18666d5477155ab
Rohit6757 <rohit.y@iksula.com>
2024-04-24 21:08:30 +0530

    merge sort

--- rohit/Leetcode/Arrays-Easy/merge.java
+++ rohit/Leetcode/Arrays-Easy/merge.java
@@ -1,7 +1,7 @@
-public class merge {
-  pu
-  public static void main(String[] args){
-    merge m = new merge();
+// public class merge {
+//   pu
+//   public static void main(String[] args){
+//     merge m = new merge();
 
-  }
-}
+//   }
+// }
