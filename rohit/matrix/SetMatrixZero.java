ef7eb480344a07951a467339b2adef750804e0a1
Rohit6757 <rohit.y@iksula.com>
2024-07-16 09:57:41 +0530

    leetcode set matrix -BruteForce approch

--- rohit/matrix/SetMatrixZero.java
+++ rohit/matrix/SetMatrixZero.java
@@ -4,7 +4,7 @@ public class SetMatrixZero {
   public static void main(String args[]){
     int matrix[][] = {
       {1,2,3},
-      {4,5,6},
+      {4,0,6},
       {7,8,9}
     };
     // System.err.println(matrix.length);
@@ -12,9 +12,30 @@ public class SetMatrixZero {
     /*
      * matrix traversal
      */
-    for(int i = 0; i < matrix.length; i++ ){
-      for(int j = 0 ;j< matrix[i].length;j++){
-        System.err.print(matrix[i][j] + " ");
+    int indexI = 0;
+    int indexJ = 0;
+    int i = 0;
+    int j = 0;
+    for(i = 0; i < matrix.length; i++ ){
+      for(j = 0 ; j < matrix[i].length; j++){
+        if(matrix[i][j] == 0){
+          System.err.print(matrix[i][j] + " at position " + i + " "+ j);
+          indexI = i;
+          indexJ = j;
+          break;
+        }
+      }
+    }
+    System.err.println(" The indexes are " + indexI + " " +indexJ);
+    i = 0 ;
+    j = 0;
+    for(i = 0 ;i < matrix.length ; i++){
+      matrix[indexI][i] = 0;
+      matrix[i][indexJ] = 0;
+    }
+    for (i = 0; i < matrix.length; i++) {
+      for (j = 0; j < matrix[i].length; j++) {
+        System.err.print(matrix[i][j] +" ");
       }
       System.err.println();
     }
